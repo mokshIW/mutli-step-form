26cba6c6832c60f91e2e0f65ddf61dbf554a4266
Moksh Keloo <moksh@integratedwebworks.com>
2025-01-15 16:49:19 -0500

    Add multi-step form component with navigation and validation

---
+++ app/multistep-form/page.tsx
@@ -0,0 +1,12 @@
+import Form from "@/components/Form";
+import React from "react";
+
+export default function MultiStepFormExample() {
+  return (
+    <div className="relative w-full  h-full flex flex-col items-center justify-start bg-gradient-to-br from-slate-900 to-neutral-800">
+      <h1 className="text-2xl font-bold py-16">Multi-Step Form with Next.js</h1>
+
+      <Form />
+    </div>
+  );
+}
---
+++ components/Form.tsx
@@ -0,0 +1,275 @@
+"use client";
+import { ArrowBigLeft, ArrowBigRight } from "lucide-react";
+import React, { useState } from "react";
+
+const steps = [
+  {
+    id: "Step 1",
+    name: "Personal Information",
+  },
+  { id: "Step 2", name: "Address" },
+  { id: "Step 3", name: "Complete" },
+];
+
+export default function Form() {
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const next = () => {
+    if (currentStep < steps.length - 1) {
+      setCurrentStep((step) => step + 1);
+    }
+  };
+
+  const prev = () => {
+    if (currentStep > 0) {
+      setCurrentStep((step) => step - 1);
+    }
+  };
+
+  return (
+    <section className="w-full h-full max-w-5xl mx-auto flex flex-col justify-start p-24 mb-20 bg-white text-black rounded-lg shadow-lg">
+      {/* steps */}
+      <nav aria-label="Progress">
+        <ol role="list" className="space-y-4 md:flex md:space-x-8 md:space-y-0">
+          {steps.map((step, index) => (
+            <li key={step.name} className="md:flex-1">
+              {currentStep > index ? (
+                <div className="group flex w-full flex-col border-l-4 border-sky-600 py-2 pl-4 transition-colors md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-sm font-medium text-sky-600 transition-colors ">
+                    {step.id}
+                  </span>
+                  <span className="text-sm font-medium">{step.name}</span>
+                </div>
+              ) : currentStep === index ? (
+                <div
+                  className="flex w-full flex-col border-l-4 border-sky-600 py-2 pl-4 md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4"
+                  aria-current="step"
+                >
+                  <span className="text-sm font-medium text-sky-600">
+                    {step.id}
+                  </span>
+                  <span className="text-sm font-medium">{step.name}</span>
+                </div>
+              ) : (
+                <div className="group flex w-full flex-col border-l-4 border-gray-200 py-2 pl-4 transition-colors md:border-l-0 md:border-t-4 md:pb-0 md:pl-0 md:pt-4">
+                  <span className="text-sm font-medium text-gray-500 transition-colors">
+                    {step.id}
+                  </span>
+                  <span className="text-sm font-medium">{step.name}</span>
+                </div>
+              )}
+            </li>
+          ))}
+        </ol>
+      </nav>
+
+      {/* Form Element */}
+      <form className="mt-12 py-12">
+        {currentStep === 0 && (
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Personal Information
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Provide your personal details.
+            </p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="firstName"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  First name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    id="firstName"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="lastName"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Last name
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    id="lastName"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Email address
+                </label>
+                <div className="mt-2">
+                  <input
+                    id="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {currentStep === 1 && (
+          <div>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Address
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Address where you can receive mail.
+            </p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              <div className="sm:col-span-3">
+                <label
+                  htmlFor="country"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Country
+                </label>
+                <div className="mt-2">
+                  <select
+                    id="country"
+                    autoComplete="country-name"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  >
+                    <option>United States</option>
+                    <option>Canada</option>
+                    <option>Mexico</option>
+                  </select>
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label
+                  htmlFor="street"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  Street address
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    id="street"
+                    autoComplete="street-address"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2 sm:col-start-1">
+                <label
+                  htmlFor="city"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  City
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    id="city"
+                    autoComplete="address-level2"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="state"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  State / Province
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    id="state"
+                    autoComplete="address-level1"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="zip"
+                  className="block text-sm font-medium leading-6 text-gray-900"
+                >
+                  ZIP / Postal code
+                </label>
+                <div className="mt-2">
+                  <input
+                    type="text"
+                    id="zip"
+                    autoComplete="postal-code"
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-sky-600 sm:text-sm sm:leading-6"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
+
+        {currentStep === 2 && (
+          <>
+            <h2 className="text-base font-semibold leading-7 text-gray-900">
+              Complete
+            </h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              Thank you for your submission.
+            </p>
+          </>
+        )}
+      </form>
+
+      {/* Form Navigation */}
+
+      <div className="mt-8 pt-5">
+        <div className="flex justify-between">
+          <button
+            type="button"
+            onClick={prev}
+            disabled={currentStep === 0}
+            className="rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50"
+          >
+            <ArrowBigLeft
+              size={24}
+              className="fill-slate-950 stroke-slate-950"
+            />
+          </button>
+          <button
+            type="button"
+            onClick={next}
+            disabled={currentStep === steps.length - 1}
+            className="rounded bg-white px-2 py-1 text-sm font-semibold text-sky-900 shadow-sm ring-1 ring-inset ring-sky-300 hover:bg-sky-50 disabled:cursor-not-allowed disabled:opacity-50"
+          >
+            <ArrowBigRight
+              size={24}
+              className="fill-slate-950 stroke-slate-950"
+            />
+          </button>
+        </div>
+      </div>
+    </section>
+  );
+}
